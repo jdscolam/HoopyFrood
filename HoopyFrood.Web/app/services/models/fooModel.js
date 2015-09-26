3677605310341776040e394c709f03b78043db35
jdscolam <jdscolam@fastmail.fm>
2015-09-26 15:46:37 -0500

    Initial Checkin

---
+++ HoopyFrood.Web/app/services/models/fooModel.js
@@ -0,0 +1,24 @@
+﻿(function ()
+{
+    'use strict';
+
+    var serviceId = 'fooModel';
+    angular.module('app').factory(serviceId, [fooModel]);
+
+    function fooModel()
+    {
+        var service = {
+            Foo: newFoo
+        };
+
+        return service;
+
+        function newFoo()
+        {
+             return {
+                 bar: 'BAR',
+                 baz: null
+             };
+        }
+    }
+})();
---
+++ HoopyFrood.Web/app/views/main/main.js
@@ -0,0 +1,39 @@
+﻿(function ()
+{
+    'use strict';
+
+    var controllerId = 'main';
+
+    angular.module('app').controller(controllerId, ['common', 'datacontext', main]);
+
+    function main(common, datacontext)
+    {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        
+        vm.title = 'Dashboard';
+
+        vm.foo = {};
+
+        vm.init = init;
+
+        activate();
+
+        function activate()
+        {
+            var promises = [init()];
+
+            common.activateController(promises, controllerId);
+        }
+
+        function init()
+        {
+            return datacontext.getFoo().then(function (data)
+            {
+                return vm.foo = data;
+            });
+        }
+    }
+})();
